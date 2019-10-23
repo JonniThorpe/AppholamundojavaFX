08a6fdd1bb0096ee69eced3509e965bd08187cda
Jonatan Thorpe Plaza <JonniThorpe7@gmail.com>
2019-10-23 12:26:52 +0200

    primer commit

---
+++ src/appholamundojavafx/Label.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appholamundojavafx;
+
+/**
+ *
+ * @author usuario
+ */
+class Label {
+
+    Label(String user_Name) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
